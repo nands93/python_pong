afc875ecddbb0af17df94a2826e5c78c162f34c8
nands93 <femmarques93+github@gmail.com>
2024-07-09 19:23:36 -0300

    added a main menu, added a new font and created menu buttons

--- pong.py
+++ pong.py
@@ -4,8 +4,7 @@ from ball import Ball
 import sys
 
 
-def draw_on_screen(screen, color1, color2, width, height, ball, p1, p2):
-	font = pygame.font.Font(None, 74)  # score font
+def draw_on_screen(screen, color1, color2, width, height, ball, p1, p2, font):
 	screen.fill(color1)  # screen color
 	pygame.draw.rect(screen, color2, p1.rect)  # player1
 	pygame.draw.rect(screen, color2, p2.rect)  # player2
@@ -32,16 +31,50 @@ def key_movements(p1, p2):
 		p2.move_down()
 
 
-def main():
-	pygame.init()
-	screen_width = 800
-	screen_height = 600
-	screen = pygame.display.set_mode((screen_width, screen_height))
-	pygame.display.set_caption('Pong')
+def menu(screen, width, height, color1, color2):
+	pygame.display.set_caption("Menu")
 
-	white = (255, 255, 255)
-	black = (0, 0, 0)
+	menu_title = pygame.font.Font('assets/font.ttf', 100)
+	title = menu_title.render("PONG", True, "#b68f40")
+	background = pygame.image.load('assets/background.png')
+	menu_button = pygame.font.Font('assets/font.ttf', 30)
+	button = pygame.image.load("assets/button.png").convert_alpha()
 
+	while True:
+		screen.blit(background, (0, 0))
+		menu_rect = title.get_rect(center=(width // 2, height // 4))
+		mouse_pos = pygame.mouse.get_pos()
+		# 1 PLAYER MODE BUTTON
+		button1 = menu_button.render("1 PLAYER MODE", True, "#d7fcd4")
+		button1_rect = button1.get_rect(center=(width // 2, height // 1.7))
+		if button1_rect.collidepoint(mouse_pos):
+			button1 = menu_button.render("1 PLAYER MODE", True, color2)
+		else:
+			button1 = menu_button.render("1 PLAYER MODE", True, "#d7fcd4")
+		# 2 PLAYERS MODE BUTTON
+		button2 = menu_button.render("2 PLAYERS MODE", True, "#d7fcd4")
+		button2_rect = button2.get_rect(center=(width // 2, height // 1.3))
+		if button2_rect.collidepoint(mouse_pos):
+			button2 = menu_button.render("2 PLAYERS MODE", True, color2)
+		else:
+			button2 = menu_button.render("2 PLAYERS MODE", True, "#d7fcd4")
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				click_pos = pygame.mouse.get_pos()
+				if button1_rect.collidepoint(click_pos):
+					main_game(screen, width, height, color1, color2)
+
+		screen.blit(title, menu_rect)
+		screen.blit(button1, button1_rect)
+		screen.blit(button2, button2_rect)
+		pygame.display.update()
+
+
+def main_game(screen, screen_width, screen_height, black, white):
 	# players
 	speed = 10
 	x_p1 = 50
@@ -61,17 +94,29 @@ def main():
 	ball = Ball(b_radius, ball_x, ball_y, b_x_speed, b_y_speed, screen_width, screen_height)
 
 	frames_per_second = pygame.time.Clock()
+	font = pygame.font.Font('assets/font.ttf', 72)
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				sys.exit()
-		draw_on_screen(screen, black, white, screen_width, screen_height, ball, player1, player2)
+		draw_on_screen(screen, black, white, screen_width, screen_height, ball, player1, player2, font)
 		key_movements(player1, player2)
 		ball.movement()
 		ball.collision(player1, player2)
-		pygame.display.update()
+		pygame.display.flip()
 		frames_per_second.tick(60)
+
+
+def main():
+	pygame.init()
+	screen_width = 800
+	screen_height = 600
+	screen = pygame.display.set_mode((screen_width, screen_height))
+	pygame.display.set_caption('Pong')
+	white = (255, 255, 255)
+	black = (0, 0, 0)
+	menu(screen, screen_width, screen_height, black, white)
 
 
 if __name__ == "__main__":
