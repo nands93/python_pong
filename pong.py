ae4ff5967c7e21eff3a634e6756111f658662da9
nands93 <femmarques93+github@gmail.com>
2024-08-21 20:00:55 -0300

    little optimization and new players

--- pong.py
+++ pong.py
@@ -6,13 +6,16 @@ import sys
 
 FONT = "assets/font.ttf"
 GOLDEN = "#b68f40"
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = "#d7fcd4"
 game_score = 0
 game_rounds = 0
 
 
 def render_text(font, string, size, color):
 	text = pygame.font.Font(font, size)
-	rendered = text.render(string, True, color)  # "#b68f40"
+	rendered = text.render(string, True, color)
 	return rendered
 
 
@@ -91,10 +94,9 @@ def configuration_menu(screen, background, color1, color2, color3, width, height
 	leg_rounds = render_text(FONT, "ROUNDS", 30, GOLDEN)
 	leg_points = render_text(FONT, "POINTS", 30, GOLDEN)
 	font_loop = pygame.font.Font(FONT, 40)
+
 	# numbers
-	round_pos = 0
 	round_num = [1, 3, 5, 7, 9]
-	score_pos = 0
 	score_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 	# buttons
@@ -104,19 +106,17 @@ def configuration_menu(screen, background, color1, color2, color3, width, height
 	add_button2 = Button(FONT, 20, "+", color2, color3, width // 1.7, height // 1.8, sound)
 	sub_button2 = Button(FONT, 20, "-", color2, color3, width // 2.5, height // 1.8, sound)
 	save_button = Button(FONT, 40, "START", color2, color3, width // 2, height // 1.2, sound)
+
 	# screen loop
+	round_pos = 0
+	score_pos = 0
 	while True:
-		if round_pos < len(round_num):
-			rounds = font_loop.render(str(round_num[round_pos]), True, GOLDEN)
-			round_rect = rounds.get_rect(center=(width // 2, height // 2.8))  # round number
-		if score_pos < len(score_num):
-			score = font_loop.render(str(score_num[score_pos]), True, GOLDEN)
-			points_rect = score.get_rect(center=(width // 2, height // 1.8))  # score number
 		screen.blit(background, (0, 0))
 		config_rect = screen_title.get_rect(center=(width // 2, height // 6))  # title
 		leg_rect1 = leg_rounds.get_rect(center=(width // 4.5, height // 2.8))  # leg_rounds
 		leg_rect2 = leg_points.get_rect(center=(width // 4.5, height // 1.8))  # leg_points
 		mouse = pygame.mouse.get_pos()
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
@@ -126,26 +126,32 @@ def configuration_menu(screen, background, color1, color2, color3, width, height
 				if back_button.rect.collidepoint(click_pos):
 					menu(screen, width, height, color1, color2, color3)
 				if add_button1.rect.collidepoint(click_pos):
-					round_pos += 1
+					round_pos = min(round_pos + 1, len(round_num) - 1)
 				if sub_button1.rect.collidepoint(click_pos):
-					if round_pos != 0:
-						round_pos -= 1
+					round_pos = max(round_pos - 1, 0)
 				if add_button2.rect.collidepoint(click_pos):
-					score_pos += 1
+					score_pos = min(score_pos + 1, len(score_num) - 1)
 				if sub_button2.rect.collidepoint(click_pos):
-					if score_pos != 0:
-						score_pos -= 1
+					score_pos = max(score_pos - 1, 0)
 				if save_button.rect.collidepoint(click_pos):
 					game_rounds = round_num[round_pos]
 					game_score = score_num[score_pos]
 					main_game(screen, width, height, color1, color2, mode)
+
+		rounds = font_loop.render(str(round_num[round_pos]), True, GOLDEN)
+		round_rect = rounds.get_rect(center=(width // 2, height // 2.8))
+		score = font_loop.render(str(score_num[score_pos]), True, GOLDEN)
+		points_rect = score.get_rect(center=(width // 2, height // 1.8))
+
 		screen.blit(screen_title, config_rect)
 		screen.blit(leg_rounds, leg_rect1)
 		screen.blit(leg_points, leg_rect2)
 		screen.blit(rounds, round_rect)
 		screen.blit(score, points_rect)
+
 		for button in [back_button, add_button1, sub_button1, add_button2, sub_button2, save_button]:
 			button.button_loop(screen, mouse)
+
 		pygame.display.update()
 
 
@@ -156,12 +162,10 @@ def main_game(screen, screen_width, screen_height, black, white, mode):
 	x_p1 = 40
 	x_p2 = screen_width - 80
 	y_player = (screen_height - 100) // 2
-	w_player = 50
-	h_player = 70
-	hit_w = 10
-	hit_h = 70
-	p1_img = "assets/*.png"
-	p2_img = "assets/*.png"
+	w_player = 25
+	h_player = 100
+	p1_img = "assets/player1.png"
+	p2_img = "assets/player2.png"
 	player1 = Player(p1_img, x_p1, y_player, speed, w_player, h_player)
 	if mode == 0:
 		player2 = AutoPlayer(p2_img, x_p2, y_player, speed, w_player, h_player)
@@ -209,10 +213,7 @@ def main():
 	screen_height = 600
 	screen = pygame.display.set_mode((screen_width, screen_height))
 	pygame.display.set_caption('Pong')
-	white = (255, 255, 255)
-	black = (0, 0, 0)
-	green = "#d7fcd4"
-	menu(screen, screen_width, screen_height, black, white, green)
+	menu(screen, screen_width, screen_height, BLACK, WHITE, GREEN)
 
 
 if __name__ == "__main__":
